185342d3de789d54c1957c38e9ac815f4e7ad6bc
Daniel Escalona <daniel07escalona@gmail.com>
2022-01-17 17:12:31 -0500

    commit tree 259bf96d9520c635de20cbdf71f50d5f4c2c72c6

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, drvr){
+    return drivers.filter( element => drvr.toLowerCase() === element.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters){
+  
+    let matchingDrivers = drivers.filter(element => {
+        let aux = element.substring(0,2);
+        if (aux === letters){
+            return element;
+        }
+
+    })
+    return matchingDrivers;
+}
+
+function matchName(drivers, drvr){
+
+    let matchingDrivers = drivers.filter(element => element['name'] === drvr)
+    return matchingDrivers;
+}
